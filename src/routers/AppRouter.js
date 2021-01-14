f147a12f4315f0f2d7a528e22f79d7cd995face5
David Cortes <caravanasar@gmail.com>
2021-01-14 19:48:37 +0100

    Added Routers

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LoginScreen from "../components/login/LoginScreen";
+import MarvelScreen from "../components/marvel/MarvelScreen";
+import { Navbar } from "../components/ui/Navbar";
+const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/" component={MarvelScreen} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default AppRouter;
