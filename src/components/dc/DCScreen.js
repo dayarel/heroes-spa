6ad81d0e648165b2b848ab538a09439166538140
David Cortes <caravanasar@gmail.com>
2021-01-15 19:32:49 +0100

    Created Heroes selector and list component

--- src/components/dc/DCScreen.js
+++ src/components/dc/DCScreen.js
@@ -1,9 +1,12 @@
 import React from "react";
+import HeroList from "../login/HeroList";
 
 const DCScreen = () => {
   return (
     <div>
       <h1>DC Screen</h1>
+      <hr />
+      <HeroList publisher="DC Comics" />
     </div>
   );
 };
--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,9 +1,12 @@
 import React from "react";
+import HeroList from "../login/HeroList";
 
 const MarvelScreen = () => {
   return (
     <div>
       <h1>Marvel Screen</h1>
+      <hr />
+      <HeroList publisher="Marvel Comics" />
     </div>
   );
 };
